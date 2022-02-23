25d86c616eae0cd58e3d3b1a8aab66a5eeefd83a
Cosmos6450 <fylhtq6450@mail.ru>
2022-02-23 20:08:56 +0300

    1. Разделил карты на <selection> и присвоил каждому <id>. 2. Присвоил всем картом <id>. 3. Сделал небольшую анимацию для карт с помощью псевдо-класса

--- js/Holod-1.js
+++ js/Holod-1.js
@@ -1,19 +1,26 @@
 const handlePlusMinus = (cardName, direction) => {
-    const counter_id = cardName + '_counter';
-    const inp = document.getElementById(counter_id);
-    if (direction === 'plus') {
-        inp.value = +inp.value + 1;
-    } else if (direction === "minus") {
-        inp.value = +inp.value - 1 > 0 ? +inp.value - 1 : 0;
-    }
+	const counter_id = cardName + '_counter';
+	const inp = document.getElementById(counter_id);
+	if (direction === 'plus') {
+		inp.value = +inp.value + 1;
+	} else if (direction === "minus") {
+		inp.value = +inp.value - 1 > 0 ? +inp.value - 1 : 0;
+	}
 }
 
+
 document.addEventListener('click', (event) => {
-    const id = event.target.id;
-    const id_parts = id.split('_');
-    if (id_parts[1] === "plus-button") {
-        handlePlusMinus(id_parts[0], 'plus');
-    } else if (id_parts[1] === "minus-button") {
-        handlePlusMinus(id_parts[0], 'minus');
-    }
+	const id = event.target.id;
+	const id_parts = id.split('_');
+	if (id_parts[1] === "plus-button") {
+		handlePlusMinus(id_parts[0], 'plus');
+	} else if (id_parts[1] === "minus-button") {
+		handlePlusMinus(id_parts[0], 'minus');
+	}
 });
+
+
+
+
+
+
